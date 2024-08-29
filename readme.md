c2a213e7ed8a1822656d4e826b3cd8dfcd8e92a3
maharjansuren37 <maharjansuren37@gmail.com>
2024-08-29 19:35:49 +0930

    first commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Frontend Environment
+
+## Webpack 
+    - webpack
+    - webpack-cli
+    - webpack-dev-server
+    - webpack-bundle-anlyzer
+    - html-webpack-plugin
+## CSS Loader and Style Loader
+## Babel
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const withReport = false;
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name][contenthash].js",
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+    ].concat(withReport ? [new BundleAnalyzerPlugin()] : []),
+    // mode: process.env.NODE_ENV === "production" ? "production" : "development"
+}
